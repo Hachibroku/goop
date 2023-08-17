53281f16c3a2afdf25679510d54b58460fb755bd
Luis <luissantana6300@gmail.com>
2023-08-17 16:04:50 -0400

    Created account models and started topic models for Endpoints

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -1,34 +1,15 @@
-from pydantic import BaseModel
-# from models import Account, AccountIn
+from api.models.accounts import AccountIn, AccountOutWithPassword
 from pymongo.errors import DuplicateKeyError
 from .client import Queries
+
 
 class DuplicateAccountError(ValueError):
     pass
 
 
-class AccountIn(BaseModel):
-    email: str
-    password: str
-    full_name: str
-
-
-class AccountOut(BaseModel):
-    id: str
-    email: str
-    full_name: str
-
-
-class AccountOutWithPassword(AccountOut):
-    hashed_password: str
-
-
 class AccountQueries(Queries):
     DB_NAME = "modjule3-project-gamma-db-1"
     COLLECTION = "accounts"
-
-    # def __init__(self, client: AsyncIOMotorClient):
-    #     super().__init__(client, self.COLLECTION)
 
     def get(self, email: str) -> AccountOutWithPassword:
         props = self.collection.find_one({"email": email})
@@ -36,7 +17,6 @@ class AccountQueries(Queries):
             return None
         props["id"] = str(props["_id"])
         return AccountOutWithPassword(**props)
-
 
     def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:
         props = info.dict()
