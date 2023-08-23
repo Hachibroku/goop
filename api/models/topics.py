22baac6bb55de5891466eb909a813b421507bf91
Murphey Osmundson <mosmundson88@gmail.com>
2023-08-23 14:29:34 -0400

    topic and voting systems initial creation, posts

--- api/models/topics.py
+++ api/models/topics.py
@@ -1,35 +1,43 @@
-# from pydantic import BaseModel
-# from typing import List, Optional
+from pydantic import BaseModel
+from typing import List, Optional
 
 
-# # MODEL FOR WHEN YOU MAKE A NEW TOPIC
-# class TopicIn(BaseModel):
-#     title: str
-#     image_url: str
-#     description: str
+# MODEL FOR WHEN YOU MAKE A NEW TOPIC
+class TopicIn(BaseModel):
+    title: str
+    image_url: str
+    description: str
 
 
-# # MODEL OF TOPIC WITH ITS ID AFTER ITS BEEN MADE
-# class TopicOut(BaseModel):
-#     id: str
-#     title: str
-#     image_url: str
-#     description: str
+# Model for casting a vote
+class Voting(BaseModel):
+    user_id: int
+    agree_count: int
+    disagree_count: int
 
 
-# # THIS MODEL SHOULD HAVE ALL THE SAME KEY:VALUE AS TopicOut
-# class SearchTopicOut(BaseModel):
-#     id: str
-#     title: str
-#     image_url: str
-#     description: str
+# MODEL OF TOPIC WITH ITS ID AFTER ITS BEEN MADE, voting is embedded
+class TopicOut(BaseModel):
+    id: str
+    title: str
+    image_url: str
+    description: str
+    voting: Voting
 
 
-# # THIS MODEL IS TO SEARCH LIST OF TOPICS BY THE PARAMETERS IN SearchTopicOut
-# class SearchTopicList(BaseModel):
-#     topics: List[SearchTopicOut]
+# THIS MODEL SHOULD HAVE ALL THE SAME KEY:VALUE AS TopicOut
+class SearchTopicOut(BaseModel):
+    id: str
+    title: str
+    image_url: str
+    description: str
 
 
-# # THIS MODEL IS TO SHOW A LIST OF ALL TOPICS
-# class TopicList(BaseModel):
-#     topics: List[TopicOut]
+# THIS MODEL IS TO SEARCH LIST OF TOPICS BY THE PARAMETERS IN SearchTopicOut
+class SearchTopicList(BaseModel):
+    topics: List[SearchTopicOut]
+
+
+# THIS MODEL IS TO SHOW A LIST OF ALL TOPICS
+class TopicList(BaseModel):
+    topics: List[TopicOut]
