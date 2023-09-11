a4473e53bf1eaf6a376f52fa6527ba16e286b900
Murphey Osmundson <mosmundson88@gmail.com>
2023-09-10 23:17:46 -0400

    New default accounts with proper hashed passwords

--- setup/mongodb/01-create-unique-account-index.js
+++ setup/mongodb/01-create-unique-account-index.js
@@ -2,13 +2,23 @@ conn = Mongo();
 db = conn.getDB("module3-project-gamma-mongo");
 
 db.accounts.insertOne({
-  email: "user@example.com",
-  password: "$2b$12$gqB.kZtNIbyKcYxStjtVTenCwLcqmUSFN/Yda2rP1znKlTHX6wukq",
-  username: "User",
+  email: "murphey@example.com",
+  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  username: "Murphey",
 });
 db.accounts.insertOne({
-  email: "user2@example.com",
-  password: "$2b$12$BnCFBYWNZI1dpQ3djPS5DuWszH3nc2v6nYPcz8OZpr6LPZSysrJty",
-  username: "User Two",
+  email: "Alex@example.com",
+  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  username: "Alex",
+});
+db.accounts.insertOne({
+  email: "Sung@example.com",
+  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  username: "Sung",
+});
+db.accounts.insertOne({
+  email: "Luis@example.com",
+  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  username: "Luis",
 });
 db.accounts.createIndex({ email: 1 }, { unique: true });
