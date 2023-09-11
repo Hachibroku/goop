edcc1f7f6dedccbe6d2781730b1771bffc0a6396
Murphey Osmundson <mosmundson88@gmail.com>
2023-09-11 14:08:25 -0400

    fixed hashed password

--- setup/mongodb/01-create-unique-account-index.js
+++ setup/mongodb/01-create-unique-account-index.js
@@ -3,22 +3,26 @@ db = conn.getDB("module3-project-gamma-mongo");
 
 db.accounts.insertOne({
   email: "murphey@example.com",
-  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  hashed_password:
+    "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
   username: "Murphey",
 });
 db.accounts.insertOne({
-  email: "Alex@example.com",
-  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  email: "alex@example.com",
+  hashed_password:
+    "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
   username: "Alex",
 });
 db.accounts.insertOne({
-  email: "Sung@example.com",
-  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  email: "sung@example.com",
+  hashed_password:
+    "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
   username: "Sung",
 });
 db.accounts.insertOne({
-  email: "Luis@example.com",
-  password: "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
+  email: "luis@example.com",
+  hashed_password:
+    "$2b$12$Xj4tYoDmfBj3dhosnpa8huPj.2r1DG/38ZSoY6YM5KT7dTw6IjOoq",
   username: "Luis",
 });
 db.accounts.createIndex({ email: 1 }, { unique: true });
